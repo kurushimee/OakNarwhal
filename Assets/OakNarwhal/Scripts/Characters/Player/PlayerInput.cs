f8aaf2ce369726395ecee98b73d68194d6aa9edc
Ivan Yermakov <iyermakov@outlook.com>
2022-06-16 16:12:49 +0700

    Add dash and fix interaction

--- Assets/OakNarwhal/Scripts/Characters/Player/PlayerInput.cs
+++ Assets/OakNarwhal/Scripts/Characters/Player/PlayerInput.cs
@@ -3,11 +3,12 @@ using UnityEngine.InputSystem;
 
 public class PlayerInput : MonoBehaviour
 {
+    private const float MoveSpeed = 5f;
     private static readonly int Horizontal = Animator.StringToHash("Horizontal");
     private static readonly int Vertical = Animator.StringToHash("Vertical");
     private static readonly int Speed = Animator.StringToHash("Speed");
-    public float moveSpeed;
     private Animator _anim;
+    private Vector3 _lastMoveDir;
     private Vector2 _movement;
     private Rigidbody2D _rb;
 
@@ -26,11 +27,47 @@ public class PlayerInput : MonoBehaviour
 
     private void FixedUpdate()
     {
-        _rb.velocity = _movement * moveSpeed;
+        _rb.velocity = _movement * MoveSpeed;
+        _lastMoveDir = _movement.normalized;
+    }
+
+    private bool CanMove(Vector3 dir, float distance)
+    {
+        return Physics2D.Raycast(transform.position + dir, dir, distance).collider == null;
+    }
+
+    private void TryMove(Vector3 baseMoveDir, float distance)
+    {
+        var moveDir = baseMoveDir;
+        var canMove = CanMove(moveDir, distance);
+
+        if (!canMove)
+        {
+            // Cannot move diagonally
+            moveDir = new Vector3(baseMoveDir.x, 0f).normalized;
+            canMove = moveDir.x != 0f && CanMove(moveDir, distance);
+            if (!canMove)
+            {
+                // Cannot move horizontally
+                moveDir = new Vector3(0f, baseMoveDir.y).normalized;
+                canMove = moveDir.y != 0f && CanMove(moveDir, distance);
+            }
+        }
+
+        if (!canMove) return;
+        _lastMoveDir = moveDir;
+        transform.position += moveDir * distance;
     }
 
     public void OnMove(InputAction.CallbackContext context)
     {
         _movement = context.ReadValue<Vector2>();
+    }
+
+    public void OnDash(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        const float dashDistance = 3f;
+        TryMove(_lastMoveDir, dashDistance);
     }
 }
--- Assets/OakNarwhal/Scripts/Characters/Player/PlayerInteraction.cs
+++ Assets/OakNarwhal/Scripts/Characters/Player/PlayerInteraction.cs
@@ -1,4 +1,5 @@
 using System;
+using JetBrains.Annotations;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -6,44 +7,42 @@ using UnityEngine.UI;
 
 public class PlayerInteraction : MonoBehaviour
 {
-    public float interactionDistance;
-    public TextMeshProUGUI interactionText;
-    public GameObject interactionHoldGo; // The UI parent to disable when not interacting
-    public Image interactionHoldProgress; // The progress bar for hold interaction type
-    private Interactable _interactable;
-    private Transform _transform;
-
-    private void Start()
-    {
-        _transform = transform;
-    }
+    [SerializeField] private float interactionDistance;
+    [SerializeField] private TextMeshProUGUI interactionText;
+    [SerializeField] private GameObject interactionHoldGo; // The UI parent to disable when not interacting
+    [SerializeField] private Image interactionHoldProgress; // The progress bar for hold interaction type
+    [SerializeField] private Transform mTransform;
 
     private void Update()
     {
-        var hit = Physics2D.Raycast(_transform.position - new Vector3(0, -0.6f, 0), _transform.up, interactionDistance);
-
         // If we hit, display interaction text
-        if (hit)
-            if (hit.collider.TryGetComponent(out Interactable interactable))
-            {
-                interactionText.text = interactable.GetDescription();
-                interactionHoldGo.SetActive(interactable.interactionType == Interactable.InteractionType.Hold);
-                _interactable = interactable;
-                return;
-            }
+        if (SearchInteractable(out var interactable))
+        {
+            interactionText.text = interactable.GetDescription();
+            interactionHoldGo.SetActive(interactable.interactionType == Interactable.InteractionType.Hold);
+            return;
+        }
 
         // If we miss, hide the UI
         interactionText.text = "";
         interactionHoldGo.SetActive(false);
     }
 
-    public void HandleInteraction(InputAction.CallbackContext context)
+    private bool SearchInteractable(out Interactable interactable)
     {
-        switch (_interactable.interactionType)
+        var hit = Physics2D.Raycast(mTransform.position, mTransform.up, interactionDistance);
+        interactable = null;
+        return hit && hit.collider.TryGetComponent(out interactable);
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        if (!SearchInteractable(out var interactable)) return;
+        switch (interactable.interactionType)
         {
             // Handle click interaction
             case Interactable.InteractionType.Click:
-                if (context.performed) _interactable.Interact();
+                if (context.started) interactable.Interact();
                 break;
 
             // Handle hold interaction
@@ -51,26 +50,22 @@ public class PlayerInteraction : MonoBehaviour
                 if (context.performed)
                 {
                     // If holding the interaction key, continue progress
-                    _interactable.IncreaseHoldTime();
-                    if (_interactable.GetHoldTime() > 1f)
+                    interactable.IncreaseHoldTime();
+                    if (interactable.GetHoldTime() >= 1f)
                     {
                         // If held long enough, interact
-                        _interactable.Interact();
-                        _interactable.ResetHoldTime();
-                        // Requires the player to stop holding the key before interacting again
-                        return;
+                        interactable.Interact();
+                        interactable.ResetHoldTime();
                     }
                 }
-                else
+                else if (context.canceled)
                 {
-                    // If stopped holding the interaction key, reset progress
-                    _interactable.ResetHoldTime();
+                    interactable.ResetHoldTime();
                 }
 
-                interactionHoldProgress.fillAmount = _interactable.GetHoldTime();
+                interactionHoldProgress.fillAmount = interactable.GetHoldTime();
                 break;
 
-            // Throw an error if interaction type is incorrect
             default:
                 throw new Exception("Unsupported type of interactable.");
         }
